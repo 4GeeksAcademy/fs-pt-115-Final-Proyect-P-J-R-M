66c007a4f2fbc0b4fc3552d3ac771747af60cbf0
Pa-ola28 <paolasuarezceron1@gmail.com>
2025-08-27 11:32:52 +0000

    post model create and score modificationd

--- src/api/models.py
+++ src/api/models.py
@@ -12,7 +12,7 @@ class User(db.Model):
     dni: Mapped [str] = mapped_column(String(120), unique=True, nullable=False)
     image: Mapped[str] = mapped_column(String(255))
     country: Mapped[str] = mapped_column(String(120))
-    score: Mapped[int] = mapped_column(default=0)
+    score: Mapped[int] = mapped_column(String(20))
 
     def serialize(self):
         return {
@@ -25,3 +25,23 @@ class User(db.Model):
             "score": self.score
             # do not serialize the password, its a security breach
         }
+    
+    class Post(db.Model):
+        id: Mapped[int] = mapped_column(primary_key=True)
+        user_id: Mapped[int] = mapped_column(db.Foreing_Key('user.id'), nullable=False)
+        destination: Mapped[str] = mapped_column(String(120))
+        description: Mapped[str] = mapped_column(String(255))
+        divisas_one: Mapped[str] = mapped_column(String(50))
+        divisas_two: Mapped[str] = mapped_column(String(50))
+
+        def serialize(self):
+            return {
+            "id": self.id,
+            "user_id": self.user_id,
+            "destination": self.destination,
+            "description": self.description,
+            "divisas_one": self.divisas_one,
+            "divisas_two": self.divisas_two
+        }
+
+    
