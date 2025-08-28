00d4583ca3e6ed6cd9d79ac7587b975ec1dfdac4
RUB3N02 <rubyerizo@gmail.com>
2025-08-28 16:56:04 +0000

    feat: create chat routes

--- src/api/routes/__init__.py
+++ src/api/routes/__init__.py
@@ -1,8 +1,11 @@
 from flask import Flask, request, jsonify, url_for, Blueprint
 from .user_routes import user_bp
 from .post_route import post_bp
+from .chat_routes import chat_bp
 
 api= Blueprint("api",__name__)
 
 api.register_blueprint(user_bp)
 api.register_blueprint(post_bp)
+api.register_blueprint(chat_bp)
+
