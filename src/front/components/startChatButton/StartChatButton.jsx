cc76a1ce157f3ea838e6ab7fd355ecb450b781da
MarcosCuriel <Marcoscuriel7@hotmail.com>
2025-09-28 16:23:20 +0000

    fix: StarChatButton and create post css

--- src/front/components/startChatButton/StartChatButton.jsx
+++ src/front/components/startChatButton/StartChatButton.jsx
@@ -6,11 +6,11 @@ export function StartChatButton({ userTwo, postId, to = "/chats", label = "Chate
     <button
       onClick={async () => {
         if (!userTwo || !postId) return;
-        const token = localStorage.getItem("token");
+        const token = localStorage.getItem("token") || sessionStorage.getItem("token");
         try {
-          await addFavoritePost(postId,token)
-        }catch (error){
-          console.error ("Error as save add", error)
+          await addFavoritePost(postId, token)
+        } catch (error) {
+          console.error("Error as save add", error)
         }
         const url = new URL(window.location.origin + to);
         url.searchParams.set("userTwo", String(userTwo));
